71903521daf5e47c38ab358649c068f587fd2a92
MateusRanzani <mateusranzani@hotmail.com>
2024-09-26 22:11:16 -0300

    init

---
+++ BOT-instagram.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time 
+from secret import pw, user
+
+class IGBot: 
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def loggin(self):
+        driver = self.driver
+        url = 'https://instagram.com/'
+        driver.get(url)
+
+        time.sleep(5)
+
+        #User
+        user_name = driver.find_element("name", "username")
+        user_name.click()
+
+        time.sleep(2)
+        user_name.send_keys(user)
+
+        #Key
+        user_password = driver.find_element("name", "password")
+        user_password.click()
+
+        time.sleep(3)
+        user_password.send_keys(pw)
+
+        #Loggin
+        user_password.send_keys(Keys.ENTER)
+        time.sleep(6)
+
+task = IGBot()
+task.loggin()
